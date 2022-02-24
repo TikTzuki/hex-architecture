aeeeb27de4f728f5da03b8ec028855ac25fe5d2a
longtpt <longtpt@minerva.vn>
2022-02-24 07:54:11 +0700

    [ADD] model relationship

--- database/models/__init__.py
+++ database/models/__init__.py
@@ -1,55 +1,93 @@
-from database.models.asset.real_estate.model import AssetRealEstate
-from database.models.asset.real_transportation.model import AssetTransportation
-from database.models.business_finance_cashflow.model import BusinessFinanceCashflow
-from database.models.business_finance_cashflow.value.model import BusinessFinanceCashflowValue
-from database.models.business_finance_cashflow.vendor.model import BusinessFinanceCashflowVendor
-from database.models.asset.other.model import AssetOther
-from database.models.collateral.model import Collateral
-from database.models.credit.card.model import CreditCard
-from database.models.credit.collateral.model import CreditCollateral
-from database.models.credit.institution.model import CreditInstitution, CreditInstitutionLoan
-from database.models.cytm_ccy_defn_master.model import CytmCcyDefnMaster
-from database.models.finance.matadata.model import FinanceMetadata, FinanceMetadataGroup, FinanceMetadataItem
-from database.models.finance.timeline.model import FinanceTimeline, FinanceTimelineAssign
-from database.models.income_squence.model import IncomeSequence
-from database.models.ma.cic_score.model import MaCicScoreDetail, MaCicScore
-
-from database.models.ma.group_cust.model import MaCreditGroupCustomerItem, MaCreditGroupCustomer
-
-from database.models.ma.card_promotion.model import MaCardPromotion
-
-from database.models.ma.question.model import MaQuestion
-
-from database.models.ma.document_type.model import MaDocumentType
-
-from database.models.ma.customer_type.model import MaCustomerType
-from database.models.partner.model import PartnerLoanProduct, Partner, PartnerProduct
-from database.models.person.credit_score.model import PersonCreditScore, PersonCreditScoreSegment
-from database.models.product.document_rule.model import ProductDocumentRule
-from database.models.product.promotion.model import ProductPromotion
-from database.models.product.question.model import ProductQuestion
-from database.models.profile.answer_cust.model import ProfileAnswerCus
-from database.models.profile.credit.card_delivery.model import ProfileCreditCardDelivery
-from database.models.profile.credit.modified.model import ProfileCreditModified
-from database.models.profile.credit.promotion.model import ProfileCreditPromotion
-from database.models.profile.credit.published.model import ProfileCreditPublished
-from database.models.profile.credit.squence_item.model import ProfileCreditSequenceItem
-from database.models.profile.credit.statement_method.model import ProfileCreditStatementMethod
-from database.models.profile.credit.supp_card.model import ProfileCreditSuppCard
-from database.models.profile.document.model import ProfileDocument
-from database.models.profile.model import Profile
-from database.models.profile.score.model import ProfileScore
-from database.models.profile.squence_item.model import ProfileSequenceItem
-from database.models.source_income.business_household.model import SourceIncomeBusinessHousehold
-from database.models.source_income.company.model import SourceIncomeCompany
-from database.models.source_income.deposit.model import SourceIncomeDeposit
-from database.models.source_income.group_asset.model import SourceIncomeGroupAsset
-from database.models.source_income.other.model import SourceIncomeOther
-from database.models.source_income.pension.model import SourceIncomePension
-from database.models.source_income.salary.model import SourceIncomeSalary
-from database.models.source_income.stock.model import SourceIncomeStock
-from database.models.sttm.regional_office.model import RegionalOffice
-from database.models.udtm_lov.model import UdtmLov
+from .asset.real_estate.model import AssetRealEstate
+from .asset.real_transportation.model import AssetTransportation
+from .business_finance_cashflow.model import BusinessFinanceCashflow
+from .business_finance_cashflow.value.model import BusinessFinanceCashflowValue
+from .business_finance_cashflow.vendor.model import BusinessFinanceCashflowVendor
+from .asset.other.model import AssetOther
+from .collateral.model import Collateral
+from .credit.card.model import CreditCard
+from .credit.collateral.model import CreditCollateral
+from .credit.institution.model import CreditInstitution, CreditInstitutionLoan
+from .cytm_ccy_defn_master.model import CytmCcyDefnMaster
+from .finance.matadata.model import FinanceMetadata, FinanceMetadataGroup, FinanceMetadataItem
+from .finance.timeline.model import FinanceTimeline, FinanceTimelineAssign
+from .income_squence.model import IncomeSequence
+from .partner.model import PartnerLoanProduct, Partner, PartnerProduct
+from .person.credit_score.model import PersonCreditScore, PersonCreditScoreSegment
+from .product.document_rule.model import ProductDocumentRule
+from .product.promotion.model import ProductPromotion
+from .product.question.model import ProductQuestion
+from .profile.answer_cust.model import ProfileAnswerCus
+from .profile.credit.card_delivery.model import ProfileCreditCardDelivery
+from .profile.credit.modified.model import ProfileCreditModified
+from .profile.credit.promotion.model import ProfileCreditPromotion
+from .profile.credit.published.model import ProfileCreditPublished
+from .profile.credit.squence_item.model import ProfileCreditSequenceItem
+from .profile.credit.statement_method.model import ProfileCreditStatementMethod
+from .profile.credit.supp_card.model import ProfileCreditSuppCard
+from .profile.document.model import ProfileDocument
+from .profile.model import Profile
+from .profile.score.model import ProfileScore
+from .profile.squence_item.model import ProfileSequenceItem
+from .source_income.business_household.model import SourceIncomeBusinessHousehold
+from .source_income.company.model import SourceIncomeCompany
+from .source_income.deposit.model import SourceIncomeDeposit
+from .source_income.group_asset.model import SourceIncomeGroupAsset
+from .source_income.other.model import SourceIncomeOther
+from .source_income.pension.model import SourceIncomePension
+from .source_income.salary.model import SourceIncomeSalary
+from .source_income.stock.model import SourceIncomeStock
+from .sttm.regional_office.model import RegionalOffice
+from .udtm_lov.model import UdtmLov
+from .collateral.cargo.model import CollCargoOwner, CollCargo
+from .collateral.machine.model import CollMachineOwner, CollMachine
+from .collateral.other.model import CollOtherOwner, CollOther
+from .collateral.paper.model import CollPaperOwner, CollPaper
+from .collateral.real_estate.apartment.model import CollReApartOwnerAuth, CollReApartOwnerCertItem, CollReApartOwnerCert, CollReApartOwner, CollReApartLandUsed, CollReApartLand, CollReApartRoom, \
+    CollReApart
+from .collateral.real_estate.land_asset.constructor import CollReLandConstItemCertItem, CollReLandConstItemCert, CollReLandConstItemOwnerAuth, CollReLandConstItemOwner, CollReLandConstItemDetail, \
+    CollReLandConstItem, CollReLandConst
+from .collateral.real_estate.land_asset.land import CollReLandUsed, CollReLandCertItem, CollReLandCert, CollReLandOwnerAuth, CollReLandOwner, CollReLand
+from .collateral.real_estate.market.model import CollReMarketOwnerAuth, CollReMarketOwner, CollReMarketCertItem, CollReMarketCert, CollReMarket
+from .collateral.right_property.model import CollRightPropertyOwner, CollRightProperty
+from .collateral.stock.model import CollRightStockOwner, CollRightStock
+from .collateral.transportation.model import CollTransOwner, CollTransLegalDocument, CollTran
+from .collateral.valuation_cert import CollPriceCertAssetAppraisal, CollPriceCertAsset, CollRe, CollPriceCert
+from .detm_clg_brn_code.model import DetmClgBrnCode
+from .loan_product.model import LoanProductPolicy, LoanProduct
+from .ma import MaBusinessLine, MaFrequence, MaAppraisalUnit, MaBankCode, MaPersonalRep, MaCollType, LosMaCollTransType, MaCostType, MaCreditCard, MaCreditProductCard, MaBusinessType, MaCustomerType, \
+    MaDocumentType, MaQuestion, MaCreditGroupCustomer, MaCardPromotion, MaCreditGroupCustomerItem, MaCollMachineLegal, MaCicScore, MaCicScoreDetail
+from .person.address.model import PersonAddress
+from .person.business.address.model import PersonBusinessAddress
+from .person.business.finance_report.model import PersonBusinessFinanceReport
+from .person.business.model import PersonBusiness
+from .person.business.warehouse.model import PersonBusinessWarehouse
+from .person.credit_institurion_relationship.model import PersonCreditInstitutionRelationship
+from .person.education.model import PersonEducation
+from .person.fcc_core.model import PersonFccCore
+from .person.group_income.model import PersonGroupIncome, PersonGroupIncomeDetail
+from .person.identity.model import PersonIdentity
+from .person.married.model import PersonMarried
+from .person.model import Person
+from .person.relationship.model import PersonalRelationship
+from .policy.detail.model import PolicyDetail
+from .policy.group.model import PolicyGroup
+from .policy.model import Policy
+from .policy.rule.model import PolicyRule
+from .profile.cost.model import ProfileCost
+from .profile.cust_group_item.model import ProfileCustGroupItem
+from .profile.exception_item.model import ProfileExceptionItem
+from .profile.income.model import ProfileIncome
+from .profile.risk_item.model import ProfileRiskItem
+from .sttm.branch.model import Branch
+from .sttm.country.model import Country
+from .sttm.district.model import District
+from .sttm.province.model import Province
+from .sttm.ward.model import Ward
+from .template.product_report.model import MaLoanProductReport
+from .template.profile_reports.model import ProfileReports
+from .template.profile_reports.reports_log.model import ProfileReportLog
+from .unknows.getb_coll_category import GetmCollCategory
 
 __all__ = [
     AssetOther,
@@ -58,20 +96,52 @@ __all__ = [
     BusinessFinanceCashflow,
     BusinessFinanceCashflowVendor,
     BusinessFinanceCashflowValue,
-    CollateralCargo,
-    CollateralMachine,
-    CollateralOther,
-    CollateralOwner,
-    CollateralPaper,
-    CollateralApartment,
-    CollateralLandAsset,
-    CollateralMarket,
-    CollateralLand,
-    CollateralRealEstate,
-    CollateralRightProperty,
-    CollateralStock,
-    CollateralTransportation,
     Collateral,
+    CollCargo,
+    CollCargoOwner,
+    CollMachine,
+    CollMachineOwner,
+    CollOther,
+    CollOtherOwner,
+    CollPaper,
+    CollPaperOwner,
+    CollReApart,
+    CollReApartRoom,
+    CollReApartLand,
+    CollReApartLandUsed,
+    CollReApartOwner,
+    CollReApartOwnerCert,
+    CollReApartOwnerCertItem,
+    CollReApartOwnerAuth,
+    CollReLandConst,
+    CollReLandConstItem,
+    CollReLandConstItemDetail,
+    CollReLandConstItemOwner,
+    CollReLandConstItemOwnerAuth,
+    CollReLandConstItemCert,
+    CollReLandConstItemCertItem,
+    CollReLand,
+    CollReLandOwner,
+    CollReLandOwnerAuth,
+    CollReLandCert,
+    CollReLandCertItem,
+    CollReLandUsed,
+    CollReMarket,
+    CollReMarketCert,
+    CollReMarketCertItem,
+    CollReMarketOwner,
+    CollReMarketOwnerAuth,
+    CollRightProperty,
+    CollRightPropertyOwner,
+    CollRightStock,
+    CollRightStockOwner,
+    CollTran,
+    CollTransLegalDocument,
+    CollTransOwner,
+    CollPriceCert,
+    CollRe,
+    CollPriceCertAsset,
+    CollPriceCertAssetAppraisal,
     CreditCard,
     CreditCollateral,
     CreditInstitution,
@@ -84,11 +154,14 @@ __all__ = [
     FinanceTimelineAssign,
     IncomeSequence,
     UdtmLov,
-    # District,
-    # Country,
-    # Branch,
-    # Province,
-    # Ward,
+    District,
+    Country,
+    Branch,
+    Province,
+    Ward,
+    MaLoanProductReport,
+    ProfileReportLog,
+    ProfileReports,
     SourceIncomePension,
     SourceIncomeStock,
     SourceIncomeSalary,
@@ -96,8 +169,7 @@ __all__ = [
     SourceIncomeGroupAsset,
     SourceIncomeDeposit,
     SourceIncomeCompany,
-    SourceIncomeBusinessHousehold,
-    Profile,
+    SourceIncomeBusinessHousehold, Profile,
     ProfileRiskItem,
     ProfileIncome,
     ProfileExceptionItem,
@@ -112,21 +184,28 @@ __all__ = [
     PersonIdentity,
     PersonGroupIncome,
     PersonFccCore,
+    PersonGroupIncomeDetail,
     PersonEducation,
     PersonCreditInstitutionRelationship,
     PersonBusiness,
     PersonBusinessFinanceReport,
+    PersonBusinessWarehouse,
     PersonBusinessAddress,
     PersonAddress,
     MaBusinessLine,
     MaFrequence,
     LoanProduct,
     LoanProductPolicy,
+    MaAppraisalUnit,
     DetmClgBrnCode,
     MaBankCode,
     GetmCollCategory,
     MaPersonalRep,
     MaCollType,
+    LosMaCollTransType,
+    MaCostType,
+    MaCreditCard,
+    MaCreditProductCard,
     MaBusinessType,
     MaCustomerType,
     Partner,
@@ -145,15 +224,18 @@ __all__ = [
     ProfileCreditPublished,
     ProfileCreditSequenceItem,
     ProfileCreditSuppCard,
+    ProfileCustGroupItem,
     MaCardPromotion,
     ProductPromotion,
     ProfileCreditCardDelivery,
     ProfileCreditPromotion,
     MaCreditGroupCustomerItem,
+    MaCollMachineLegal,
     PersonCreditScoreSegment,
     PersonCreditScore,
     MaCicScore,
     MaCicScoreDetail,
     RegionalOffice,
     ProfileCreditStatementMethod
+
 ]
